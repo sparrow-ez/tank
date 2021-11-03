85388d6cb911cb6b9081cdf09a0a936f719528d0
gongyuxing <2990520151@qq.com>
2021-11-03 15:30:54 +0800

    抽象出tank类，封装tank类方向方法 设置TankFrame类的方向

--- src/com/mashibing/tank/TankFrame.java
+++ src/com/mashibing/tank/TankFrame.java
@@ -8,12 +8,8 @@ import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
 
-    //坦克的size
-    int x = 200, y = 200;
-    //tank运动方向
-    Dir dir = Dir.DOWN;
-    //坦克运动速度
-    private static final int SPEED = 10;
+    Tank myTank = new Tank(200, 200, Dir.DOWN);
+
 
     public TankFrame() {
         setSize(800, 600);
@@ -35,22 +31,8 @@ public class TankFrame extends Frame {
     @Override
     public void paint(Graphics g) {
 
-        g.fillRect(x, y, 50, 50);
+        myTank.paint(g);
 
-        switch (dir) {
-            case LEFT:
-                x -= SPEED;
-                break;
-            case UP:
-                y -= SPEED;
-                break;
-            case RIGHT:
-                x += SPEED;
-                break;
-            case DOWN:
-                y += SPEED;
-                break;
-        }
     }
 
     class MyKeyListener extends KeyAdapter {
@@ -109,10 +91,10 @@ public class TankFrame extends Frame {
         }
 
         private void setMainTankDir() {
-            if (bL) dir = Dir.LEFT;
-            if (bU) dir = Dir.UP;
-            if (bR) dir = Dir.RIGHT;
-            if (bD) dir = Dir.DOWN;
+            if (bL) myTank.setDir(Dir.LEFT);
+            if (bU) myTank.setDir(Dir.UP);
+            if (bR) myTank.setDir(Dir.RIGHT);
+            if (bD) myTank.setDir(Dir.DOWN);
 
         }
 
