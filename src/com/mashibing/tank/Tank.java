ce4f86ee8827b260df786c87a094e36efff09919
gongyuxing <2990520151@qq.com>
2021-11-03 17:16:22 +0800

    双缓冲解决闪烁问题

--- src/com/mashibing/tank/Tank.java
+++ src/com/mashibing/tank/Tank.java
@@ -55,7 +55,10 @@ public class Tank {
     }
 
     public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.YELLOW);
         g.fillRect(x, y, 50, 50);
+        g.setColor(c);
 
         move();
 
--- src/com/mashibing/tank/TankFrame.java
+++ src/com/mashibing/tank/TankFrame.java
@@ -9,12 +9,13 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame {
 
     Tank myTank = new Tank(200, 200, Dir.DOWN);
-
     Bullet bullet = new Bullet(300,300,Dir.DOWN);
+    static final int GAME_WIDTH = 800,GAME_HEIGHT=600;
+
 
 
     public TankFrame() {
-        setSize(800, 600);
+        setSize(GAME_WIDTH, GAME_HEIGHT);
         setResizable(false);
         setTitle("tank war");
         setVisible(true);
@@ -28,6 +29,25 @@ public class TankFrame extends Frame {
                 System.exit(0);
             }
         });
+    }
+
+    /**
+     * 双缓冲解决闪烁问题
+     */
+    Image offScreenImage = null;
+    @Override
+    public void update(Graphics g){
+        if (offScreenImage == null){
+            offScreenImage = this.createImage(GAME_WIDTH,GAME_HEIGHT);
+        }
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.BLACK);
+        gOffScreen.fillRect(0,0,GAME_WIDTH,GAME_HEIGHT);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage,0,0,null);
+
     }
 
     @Override
